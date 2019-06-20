b11c753535f032288fd721bf89d23a8af072d8bf
new <42086901+1artem2@users.noreply.github.com>
2019-06-20 13:43:27 +0300

    Редактирование script.js

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
-let notes = {};
-
 let text = document.getElementById("text");
 let butt = document.getElementById("butt");
+let list = document.getElementById("list");
+
+let notes = {};
+let edit = false;
+let key;
 
 butt.addEventListener("click", function() {
-  let date = new Date();
-  let now =
-    date.getHours() +
-    ":" +
-    date.getMinutes() +
-    ":" +
-    date.getSeconds() +
-    ":" +
-    date.getDay() +
-    "." +
-    date.getMonth() +
-    "." +
-    date.getFullYear();
-  notes[now] = text.value;
+  if (edit) {
+    notes[key] = text.value;
+    edit = false;
+    key = undefined;
+    text.value = " ";
+  } else {
+    let date = new Date();
+    let now =
+      addZero(date.getHours()) +
+      ":" +
+      addZero(date.getMinutes()) +
+      ":" +
+      addZero(date.getSeconds()) +
+      "  " +
+      addZero(date.getDate()) +
+      "." +
+      addZero(date.getMonth() + 1) +
+      "." +
+      date.getFullYear();
+    notes[now] = text.value;
+    text.value = " ";
+
+    let li = document.createElement("li");
+    li.innerHTML = now;
+    list.appendChild(li);
+
+    li.addEventListener("click", function() {
+      text.value = notes[this.innerHTML];
+      edit = true;
+      key = this.innerHTML;
+    });
+  }
 });
+
+function addZero(num) {
+  if (num >= 0 && num <= 9) {
+    return "0" + num;
+  } else {
+    return num;
+  }
+}
